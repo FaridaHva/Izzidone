358bda7ca5c7302f7da24d395855b7be8212b031
FaridaHva <faridahva20@gmail.com>
2023-08-25 15:58:17 +0400

    Added Orders

--- izzidone/services/models.py
+++ izzidone/services/models.py
@@ -140,19 +140,19 @@ class AllPros(models.Model):
 
 
 class Order(models.Model):
-    title=models.ForeignKey("services.SubService", max_length=50, verbose_name="Title", on_delete=models.CASCADE)
+    title = models.ForeignKey("services.SubService", verbose_name="Title", related_name="title_orders", on_delete=models.CASCADE)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, verbose_name="User")
-    subService = models.ForeignKey("services.SubService", verbose_name="Subservice", on_delete=models.CASCADE)
-    options = models.ManyToManyField(verbose_name="Options")
-    address = models.CharField(verbose_name="Street Address")
+    subService = models.ForeignKey("services.SubService", verbose_name="Subservice", related_name="subservice_orders", on_delete=models.CASCADE)
+    options = models.ManyToManyField("services.AllPros", verbose_name="Options")
+    address = models.CharField(verbose_name="Street Address", max_length=50)
     start_date = models.DateTimeField(verbose_name="Select Start Date")
-    starting_time = models.DateTimeField(verbose_name="Select Start Date")
+    starting_time = models.DateTimeField(verbose_name="Select Starting Time")
     service_detail = models.TextField(verbose_name="Service Details")
-    professional = models.ForeignKey("services.Professionals", on_delete=models.CASCADE, verbose_name="Professional")
+    professional = models.ForeignKey("services.Professional", on_delete=models.CASCADE, verbose_name="Professional")
     upload_image = models.ImageField(verbose_name="Upload Photo")
     created_at = models.DateTimeField("Created At", auto_now_add=True)
     is_active = models.BooleanField("Is Active", default=True)
-    price = models.DecimalField(verbose_name="Total Price", default=0)
+    price = models.IntegerField(verbose_name="Total Price", default=0)
 
 
     class Meta:
@@ -161,3 +161,6 @@ class Order(models.Model):
 
     def __str__(self):
         return self.title      
+    
+
+
