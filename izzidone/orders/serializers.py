b285452393cbf45f8c9c6fc3fe169b9c423bdb64
FaridaHva <faridahva20@gmail.com>
2023-08-25 15:59:49 +0400

    Added Orders

--- izzidone/orders/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Order
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        exclude = ('created_at', 'is_active')
-
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
--- izzidone/orders/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import OrderListCreateAPIView
-
-urlpatterns = [
-    path('orders/', OrderListCreateAPIView.as_view(), name='order-list-create'),
-]
