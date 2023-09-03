b31fc632ae0ecc4f3304f3de845545134808bd96
FaridaHva <faridahva20@gmail.com>
2023-09-04 01:52:52 +0400

    Completed Services

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\farid\Desktop\IZZI\izzi\venv
+set VIRTUAL_ENV=C:\Users\farid\Desktop\IZZI\Izzidone\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
